012af5a0e0661ba45465e9e10d6088d823e63b79
Rishabh Khadse <rishabhmk25@gmail.com>
2026-05-28 13:39:58 +0530

    Fix: extension

--- extension/.plasmo/static/common/react.js
+++
@@ -1,8 +0,0 @@
-import { Fragment } from "react";
-
-export const getLayout = (RawImport) =>
-  typeof RawImport.Layout === "function"
-    ? RawImport.Layout
-    : typeof RawImport.getGlobalProvider === "function"
-      ? RawImport.getGlobalProvider()
-      : Fragment;
